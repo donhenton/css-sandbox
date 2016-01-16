5c5e016b446c4f1c7b68a904b0ef0adc5e33ade0
donhenton@gmail.com <>
2016-01-16 10:52:00 -0600

    setting up dropbox demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,49 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 module.exports = function (grunt) {
-    // Project configuration.
+    // require("load-grunt-tasks")(grunt); // npm install --save-dev load-grunt-tasks
+    //https://github.com/gruntjs/grunt-contrib-watch/blob/master/docs/watch-examples.md
     grunt.initConfig({
+        sass: {
+            droptest: {
+                options: {
+                    style: 'expanded',
+                    lineNumbers: true
+                },
+                files: {
+                    './droptest/css/droptest.css': './droptest/sass/droptest.scss'
+                }
+            }
+        },
+        watch: {
+            options: {livereload: true}
+            ,
+            drop_test: {
+                files: [ './droptest/*.js',
+                    './droptest/css/droptest.css',
+                    './droptest/droptest.html'],
+                tasks: ['sass:droptest'],
+                options: {
+                    spawn: true
+                }
+            },
+        },
+        connect: {
+            droptest: {
+                options: {
+                    port: 8888,
+                    hostname: '*',
+                    open: 'http://localhost:8888/droptest.html',
+                    livereload: true,
+                    base: 'droptest'
+                }
+            }
+        }
     });
-};
+
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask("droptest", ['sass:droptest','connect:droptest', 'watch:drop_test']);
+
+}
