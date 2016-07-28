e6da6efbc2cc0dec98446cf7209d3b80636901b9
Don Henton <donhenton@gmail.com>
2016-07-28 09:57:26 -0500

    working on vertical menu 3

--- demos/vert-menu/js/menucode.js
+++ demos/vert-menu/js/menucode.js
@@ -1,6 +1,6 @@
  $(document).ready(function(){
      
-     $("div.topLevel.navBeta, div.topLevel.navGamma").unbind("click").bind("click", function(evt) {
+     $(".topLevel.navBeta, .topLevel.navGamma").unbind("click").bind("click", function(evt) {
 		evt.stopPropagation();
 		if ($(this).hasClass("expanded")) {
 			 $(this).removeClass("expanded");
