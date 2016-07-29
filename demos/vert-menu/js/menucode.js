89bbd93a17178e7a8ce910d4b4d7f7f197dae6e6
Don Henton <donhenton@gmail.com>
2016-07-29 14:03:24 -0500

    added sticky header demo

--- demos/vert-menu/js/menucode.js
+++ demos/vert-menu/js/menucode.js
@@ -8,6 +8,29 @@
 			 $(this).addClass("expanded");
 		}
 	});
+        
+        
+         $(document).off('scroll.demo-ns').on('scroll.demo-ns', function () {
+            var st = Number($(document).scrollTop());
+               // console.log("zz "+st)
+              if (st > 100) {
+                 $('.sticky-header').addClass('sticky-header-gone') ;
+             } else {
+                 $('.sticky-header').removeClass('sticky-header-gone');
+            }
+//
+//
+//            if (st > 450 && self.enabled) {
+//                $('body').addClass('lock-header-2');
+//            } else {
+//                $('body').removeClass('lock-header-2');
+//            }
+        });
+     
+        
+        
+        
+        
      
      
  });
